a4a21b0605d30acf7d5aaeb5a08f110b91591828
jimmynguyen1308 <jimmynguyen1308@gmail.com>
2023-02-19 16:40:43 +1000

    setup Apollo Server

---
+++ src/pages/api/graphql.ts
@@ -0,0 +1,24 @@
+import { ApolloServer } from "@apollo/server"
+import {
+  ApolloServerPluginLandingPageLocalDefault,
+  ApolloServerPluginLandingPageProductionDefault,
+} from "@apollo/server/plugin/landingPage/default"
+import { startServerAndCreateNextHandler } from "@as-integrations/next"
+
+import { typeDefs } from "./typeDefs"
+import { resolvers } from "./resolvers"
+
+const apolloServer = new ApolloServer({
+  typeDefs,
+  resolvers,
+  plugins: [
+    process.env.NODE_ENV === "production"
+      ? ApolloServerPluginLandingPageProductionDefault({
+          graphRef: "my-graph-id@my-graph-variant",
+          footer: false,
+        })
+      : ApolloServerPluginLandingPageLocalDefault({ footer: false }),
+  ],
+})
+
+export default startServerAndCreateNextHandler(apolloServer)
